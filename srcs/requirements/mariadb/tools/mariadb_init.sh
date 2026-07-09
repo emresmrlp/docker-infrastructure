a8902a27258b280b95bf75b8be5bbf86f6b6d954
emresmrlp <emresmrlp@gmail.com>
2026-07-09 17:42:38 +0300

    feat(mariadb): init.sh added. db pass env added to Dockerfile

--- srcs/requirements/mariadb/tools/mariadb_init.sh
+++ srcs/requirements/mariadb/tools/mariadb_init.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+MYSQL_PASS=$(cat /run/secrets/db_password | tr -d '\n')
+MYSQL_ROOT_PASS=$(cat /run/secrets/db_root_password | tr -d '\n')
+
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "Mariadb already set up!"
+    echo "MariaDB: Database will be set up..."
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    service mysql start
+
+    mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+    mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASS}';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASS}';"
+    mysql -e "FLUSH PRIVILEGES;"
+
+    mysqladmin -uroot -p"${MYSQL_ROOT_PASS}" shutdown
+    echo "MariaDB: Database is set up."
+else
+    echo "MariaDB: Database already set up. Skipped..."
 fi
 
 exec "$@"
